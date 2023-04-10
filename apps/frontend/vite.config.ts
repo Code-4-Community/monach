e4aaf427877ced1cd8c8ec034eb769a04b03d9d7
Ryan Jung <ryan.matthew.jung@gmail.com>
2023-04-10 14:46:38 -0400

    🐛 fix: polyfill global

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -6,6 +6,10 @@ import viteTsConfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/frontend',
 
+  define: {
+    global: {},
+  },
+
   server: {
     port: 4200,
     host: 'localhost',
